b84fd760c7ab1df91c9aa251dd3370a2b2b6bc29
sohit kumar <sohitkumar9548@gmail.com>
2026-08-29 18:13:01 +0530

    origin main

--- src/db/db.js
+++ src/db/db.js
@@ -1,8 +1,12 @@
 const mongoose = require("mongoose");
 
 async function connectDB() {
-  mongoose.connect(process.env.MONGO_URI);
-  console.log("connected to DB");
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("connected to DB");
+  } catch (err) {
+    console.log("Database connection error", err);
+  }
 }
 
 module.exports = connectDB;
