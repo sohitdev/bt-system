4ca30cbf3890c218b3b9f55fef69ad5ec27d6aee
sohit kumar <sohitkumar9548@gmail.com>
2026-08-30 12:10:30 +0530

    implemented login api

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 const userModel = require("../models/user.model");
 const jwt = require("jsonwebtoken");
+// const bcrypt = require("bcryptjs");
 
 async function register(req, res) {
   try {
@@ -40,4 +41,45 @@ async function register(req, res) {
   }
 }
 
-module.exports = { register };
+async function login(req, res) {
+  try {
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({ email }).select("+password");
+
+    if (!user) {
+      return res
+        .status(401)
+        .json({ message: "User not found", status: "login failed" });
+    }
+
+    const isPasswordValid = await user.comparePassword(password);
+
+    if (!isPasswordValid) {
+      return res
+        .status(401)
+        .json({ message: "Invalid password", status: "login failed" });
+    }
+
+    const token = jwt.sign({ userID: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "3d",
+    });
+
+    res.cookie("token", token);
+
+    res.status(200).json({
+      message: "User logged in successfully",
+      status: "success",
+      token,
+      user: {
+        id: user._id,
+        email: user.email,
+        name: user.name,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message, status: "login failed" });
+  }
+}
+
+module.exports = { register, login };
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -4,6 +4,6 @@ const authController = require("../controllers/auth.controller");
 const router = express.Router();
 
 router.post("/register", authController.register);
-// router.post("/login", login);
+router.post("/login", authController.login);
 
 module.exports = router;
